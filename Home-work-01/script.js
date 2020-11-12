9126a605c73319a71fea2b3d129e48f779e5e308
Volodymyr Sus <volodymyrsus33@gmail.com>
2020-11-12 20:58:38 +0200

    Some little part of this HW

--- Home-work-01/script.js
+++ Home-work-01/script.js
@@ -1,3 +1,6 @@
 let bread = 15.678, fish = 123.965, water = 90.2345, sum;
 document.writeln ("Максимальна ціна - ", Math.max (bread, fish, water), "<br>");
 document.writeln ("Мінімальна ціна - ", Math.min(bread, fish, water), "<br>");
+sum = bread + fish + water;
+document.writeln ("Сума всіх товарів - ", sum, "<br>");
+document.writeln ("Сума цілих частин округлених до меншого - ", Math.floor(bread) + Math.floor(fish) + Math.floor(water), "<br>");
