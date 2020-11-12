6cbe09a3f9ad5fb23e2dff0ee0d18995a0d0026d
Volodymyr Sus <volodymyrsus33@gmail.com>
2020-11-12 21:43:12 +0200

    Some part of HW

--- Home-work-01/script.js
+++ Home-work-01/script.js
@@ -1,6 +1,11 @@
 let bread = 15.678, fish = 123.965, water = 90.2345, sum;
-document.writeln ("Максимальна ціна - ", Math.max (bread, fish, water), "<br>");
-document.writeln ("Мінімальна ціна - ", Math.min(bread, fish, water), "<br>");
+document.writeln ("Максимальна ціна - ", Math.max (bread, fish, water), "<br>", "<br>");
+document.writeln ("Мінімальна ціна - ", Math.min(bread, fish, water), "<br>", "<br>");
 sum = bread + fish + water;
-document.writeln ("Сума всіх товарів - ", sum, "<br>");
-document.writeln ("Сума цілих частин округлених до меншого - ", Math.floor(bread) + Math.floor(fish) + Math.floor(water), "<br>");
+document.writeln ("Сума всіх товарів - ", sum, "<br>", "<br>");
+document.writeln ("Сума цілих частин округлених до меншого - ", Math.floor(bread) + Math.floor(fish) + Math.floor(water), "<br>", "<br>");
+let sumRoundedToHundreds; //По-іншому не додумався як це зробити
+sumRoundedToHundreds = (Math.ceil (bread) + Math.ceil (fish) + Math.ceil (water)) / 100;
+document.writeln ("Cума товарів округлена до сотень - ",  Math.floor (sumRoundedToHundreds) * 100 , "<br>", "<br>");
+document.writeln ("Булеве значення: чи є сума (округлена в меншу сторону) парним чи непарним числом? (true - парне, false - непарне) - ", Math.floor(bread + fish + water) %2 !== 0 , "<br>", "<br>");
+document.writeln ("Cума решти, при оплаті всіх товарів сумою 500 грн - " , 500 - sum.toFixed(2) , "<br>", "<br>");
