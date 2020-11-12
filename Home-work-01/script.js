b96e85e35dc8dd966d947285f78dcb8c7f38acca
Volodymyr Sus <volodymyrsus33@gmail.com>
2020-11-12 22:34:02 +0200

    Some part of HW

--- Home-work-01/script.js
+++ Home-work-01/script.js
@@ -6,6 +6,7 @@ document.writeln ("Сума всіх товарів - ", sum, "<br>", "<br>");
 document.writeln ("Сума цілих частин округлених до меншого - ", Math.floor(bread) + Math.floor(fish) + Math.floor(water), "<br>", "<br>");
 let sumRoundedToHundreds; //По-іншому не додумався як це зробити
 sumRoundedToHundreds = (Math.ceil (bread) + Math.ceil (fish) + Math.ceil (water)) / 100;
-document.writeln ("Cума товарів округлена до сотень - ",  Math.floor (sumRoundedToHundreds) * 100 , "<br>", "<br>");
+document.writeln ("Cума товарів округлена до сотень - ",  Math.floor (sumRoundedToHundreds) * 100, "<br>", "<br>");
 document.writeln ("Булеве значення: чи є сума (округлена в меншу сторону) парним чи непарним числом? (true - парне, false - непарне) - ", Math.floor(bread + fish + water) %2 !== 0 , "<br>", "<br>");
 document.writeln ("Cума решти, при оплаті всіх товарів сумою 500 грн - " , 500 - sum.toFixed(2) , "<br>", "<br>");
+document.writeln ("Cереднє значення цін, округлене до другого знаку після коми - ", (sum / 3).toFixed(2), "<br>", "<br>");
