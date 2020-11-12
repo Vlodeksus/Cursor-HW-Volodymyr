56c4f8b01091b000f2cb80dd2daa50270a8ef008
Volodymyr Sus <volodymyrsus33@gmail.com>
2020-11-12 18:18:24 +0200

    Change name of file

--- Home work 01/script.js
+++
@@ -1,3 +0,0 @@
-let bread = 15.678, fish = 123.965, water = 90.2345, sum;
-document.writeln ("Максимальна ціна - ", Math.max (bread, fish, water), "<br>");
-document.writeln ("Мінімальна ціна - ", Math.min(bread, fish, water), "<br>");
