c05bedf337234ec151acddd937b5c4067f6ed5c1
Volodymyr Sus <volodymyrsus33@gmail.com>
2020-11-29 13:55:54 +0200

    HW - function

--- Home-work-03/script.js
+++ Home-work-03/script.js
@@ -1,10 +1,39 @@
-//Знаходить найбільше число з чотирьох. 
+// //Знаходить найбільше число з чотирьох (Це я спочатку не знав як роздылити число на цифри, тому зробив числами) 
 function getMaxDigit (a,b, c, d) {
         return Math.max(a, b, c, d);
 }
-document.writeln (`<h3>Найбільше число з чотирьох введених: ${getMaxDigit (5, 6, 7 ,8 )}</h3><br>`);
+document.writeln (`<h3>(Cпочатку зробив так бо, ну ніяк не виходило знайти цифру в числі)<br>
+Найбільше число з чотирьох введених: ${getMaxDigit (5, 6, 7 ,8 )}</h3><br>`);
 
-//Генерація паролю
+// // Цифра з найбільшим значенням в числі
+function getMaxDigit2 (digit) {
+    splitedDigit = digit.toString().split('');
+    maxDigit = 0;
+
+    splitedDigit.forEach(elements => {
+        if (+elements > maxDigit) {
+            maxDigit = elements;
+        }
+    });
+    return maxDigit;
+}
+document.writeln(`<h3>Цифра з найбільшим значенням в числі - ${getMaxDigit2 (123457)}</h3><br>`);
+
+//Степінь числа
+
+function degreeOfNumber (digit, degree) {
+let digitDegree = 1;
+
+    for (let i = 0; i < degree; i++) {
+        
+        digitDegree *= digit;
+        
+    } 
+    return digitDegree
+}
+document.writeln (`<h3>Число в степені n становить -  ${degreeOfNumber(3, 3)}</h3><br>`);
+
+// //Генерація паролю
 function getRandomPassword (length) {
     let password = "";
     let symbols = "0123456789";
@@ -15,7 +44,7 @@ return password;
 }
 document.writeln (`<h3> Пароль з 8 цифр: ${getRandomPassword(8)} </h3><br>`);
 
-//Перша літера слова велика, решта малі: 
+// //Перша літера слова велика, решта малі: 
 function firstBigLetter (word) {
 let allLittleLetters = word.toLowerCase();
 let firstLetter = word [0].toUpperCase();
@@ -23,16 +52,16 @@ return firstLetter + allLittleLetters.slice(1);
 }
 document.writeln (`<h3>Перша велика літера у слові: ${firstBigLetter("ВоЛоДИМир")}</h3><br>`);
 
-//Випадкове число від значення1 до значення2
+// //Випадкове число від значення 1 до значення 2
 function getRandomNumber (digit1, digit2) {
 let randomDigit = digit1 + Math.random() * (digit2 + 1 - digit1);
 return Math.floor(randomDigit)
 }
-document.writeln (`<h3>Випадкове число від значення1 до значення2 становить - ${getRandomNumber(3, 9)}</h3><br>`);
+document.writeln (`<h3>Випадкове число від значення 1 до значення 2 становить - ${getRandomNumber(3, 9)}</h3><br>`);
 
-//Зарплата-податок
+// //Зарплата-податок
 function zarplataBezPodatku (zarplata) {
 bezPodatku = zarplata - (zarplata * (19.5/100));
-return bezPodatku;
+return bezPodatku.toFixed(2);
 } 
-document.writeln (`<h3>Зарплата без податку (-19,5%) становить - ${zarplataBezPodatku(1000)}</h3>`)
+document.writeln (`<h3>Зарплата без податку (-19,5%) становить - ${zarplataBezPodatku(987.99)}</h3>`)
