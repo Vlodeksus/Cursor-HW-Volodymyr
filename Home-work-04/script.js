5f62b43bfea6bac1dbc08c0f55a5af11e4e90e0a
Volodymyr Sus <volodymyrsus33@gmail.com>
2020-12-14 14:27:05 +0200

    HW - array

--- Home-work-04/script.js
+++ Home-work-04/script.js
@@ -2,28 +2,6 @@ const students = ["Олександр", "Ігор", "Олена", "Іра", "О�
 const themes = ["Диференційне рівняння", "Теорія автоматів", "Алгоритми і структури даних"];
 const marks = [4, 5, 5, 3, 4, 5];
 
-// function pairsOfStudents(students) {
-//     let pairs = [];
-//     let girls = [];
-//     for (let i = 0; i < students.length; i++) {
-//         let nameOfStudent = students[i];
-//         if (nameOfStudent[nameOfStudent.length - 1] !== ‘а’) {
-//             pairs.push([nameOfStudent]);
-//         } else {
-//             girls.push(nameOfStudent);
-//         }
-//     }
-//     for (let i = 0; i < pairs.length; i++) {
-//         if (i < girls.length) {
-//             pairs[i].push(girls[i]);
-//         }
-//     }
-//     const results = pairs.reduce((result, pair) => {
-//         return result + pair.join(” - “) + “;”;
-//     }, “”);
-//     return results.split(“;”);
-// }
-// console.log(`<h3>Розділення студентів на пари - ${pairsOfStudents(students).join(“; “)}</h3><br>`)
 //Пари студентів
 function pairsOfStudents(students) {
 
